e241c44337ebf9fa08bdccd15e9086b0a53d5b9e
Andrey Ignatov <hiller994@gmail.com>
2025-03-22 23:42:49 +0300

    селеноид: - управление версиями - скрытие данных для авторизации

--- conftest.py
+++ conftest.py
@@ -1,22 +1,36 @@
+import os
+
 import pytest
-from selene import browser, be, have
+from selene import browser, be, have, Config, Browser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options #Options нужен для кастомного брауера
+from dotenv import load_dotenv
 
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
-def setup_browser():
-    #browser.config.window_height = 1080  # высота браузера
-    #browser.config.window_width = 1920  # ширина браузера
-    #browser.open('https://duckduckgo.com/')
-    browser.config.base_url = 'https://azs.tatneft.ru'
+#Версия браузера
+DEFAULT_BROWSER_VERSION = '128.0' #константа для дефолтной версии браузера
+def pytest_addoption(parser):
+    parser.addoption( #через парсер зачитываем наши опции
+        '--browser_version', #выбор версии браузера
+        default='100.0' #по дефолту
+    )
 
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+@pytest.fixture(scope='function') #autouse=True нужен, чтобы не указывать руками фикстуру в тестах
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION #это защитит от того, что не заполнили параметр
+
+#запуск браузер селеноид
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "128.0",
+        "browserName": "chrome", # browser_name вместо хрома
+        "browserVersion": browser_version, #вместо "100.0"
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -24,15 +38,21 @@ def setup_browser():
     }
 
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser = Browser(Config(driver=driver)) #мы создаем свой объект браузера. В классе браузера передаем конструктор Config, в конструктор Config передаем наш driver
 
-    browser.config.driver = driver
 
-    yield
+    yield browser
     attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
     attach.add_screenshot(browser)
+
     browser.quit()
